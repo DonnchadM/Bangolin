9e5b3b671b9da92b59638e6e6245154301b0732d
DonnchadM <40298332@ads.qub.ac.uk>
2024-11-05 22:53:31 +0000

    coin ui

--- Bangolin/Assets/System.cs
+++ Bangolin/Assets/System.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameSystem : MonoBehaviour
 {
@@ -7,10 +10,13 @@ public class GameSystem : MonoBehaviour
     private static GameSystem instance;
     private int levelsBeat = 0;
     private SceneChanger sceneChanging;
+    private GameObject canvas;
+    // Dictionary to store power-ups and their quantities
+    private Dictionary<string, int> powerUpInventory = new Dictionary<string, int>();
+    public GameObject coinText;
 
     void Awake()
     {
-        
         if (instance != null && instance != this)
         {
             Destroy(gameObject);
@@ -20,27 +26,83 @@ public class GameSystem : MonoBehaviour
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
-    public int getCoins(){
+
+    public int getCoins()
+    {
         return coins;
+        coinText.GetComponent<TMP_Text>().text = coins.ToString();
+
     }
-    public void addCoin(int coinsAdd){
+
+    public void addCoin(int coinsAdd)
+    {
         coins += coinsAdd;
+        coinText.GetComponent<TMP_Text>().text = coins.ToString();
     }
-    public int getExtraLives(){
+
+    public void takeCoins(int coinsTake){
+        coins -= coinsTake;
+        coinText.GetComponent<TMP_Text>().text = coins.ToString();
+    }
+
+    public int getExtraLives()
+    {
         return extraLives;
     }
-    public void addExtraLives(int extraLivesAdd){
+
+    public void addExtraLives(int extraLivesAdd)
+    {
         extraLives += extraLivesAdd;
     }
-    public void beatLevel(){
+
+    public void beatLevel()
+    {
         levelsBeat++;
         sceneChanging.sceneToChange = "Shop";
         sceneChanging.ChangeScene();
     }
+
+    public void addPowerUp(string powerUpName, int quantity = 1)
+    {
+        if (powerUpInventory.ContainsKey(powerUpName))
+        {
+            powerUpInventory[powerUpName] += quantity;
+        }
+        else
+        {
+            powerUpInventory[powerUpName] = quantity;
+        }
+    }
+
+    public int getPowerUpQuantity(string powerUpName)
+    {
+        if (powerUpInventory.ContainsKey(powerUpName))
+        {
+            return powerUpInventory[powerUpName];
+        }
+        return 0; // Return 0 if the power-up is not in the inventory
+    }
+
+    public bool usePowerUp(string powerUpName)
+    {
+        if (powerUpInventory.ContainsKey(powerUpName) && powerUpInventory[powerUpName] > 0)
+        {
+            powerUpInventory[powerUpName]--;
+            if (powerUpInventory[powerUpName] == 0)
+            {
+                powerUpInventory.Remove(powerUpName);
+            }
+            return true; // Indicate that the power-up was successfully used
+        }
+        return false; // Indicate that the power-up is not available or has no quantity left
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         sceneChanging = GetComponent<SceneChanger>();
+        canvas = GameObject.Find("Canvas");
+        coinText = GameObject.Find("coins");
     }
 
     // Update is called once per frame
